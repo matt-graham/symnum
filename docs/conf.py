6052c42b91edec85d657c948a376ef3693d27217
Matt Graham <matthew.m.graham@gmail.com>
2023-08-11 17:49:56 +0100

    Add Sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "SymNum"
+copyright = "2023, Matt Graham"
+author = "Matt Graham"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+]
+
+napoleon_preprocess_types = True
+autoclass_content = "both"
+python_use_unqualified_type_names = True
+
+autodoc_typehints = "description"
+
+autodoc_default_options = {"inherited-members": True, "special-members": "__call__"}
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "sympy": ("https://docs.sympy.org/latest/", None),
+}
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
