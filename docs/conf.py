c2ce85bf7288e5ba1468dad19325b1d87cfef432
Matt Graham <matthew.m.graham@gmail.com>
2023-08-14 15:38:35 +0100

    Add Numba intersphinx mapping

--- docs/conf.py
+++ docs/conf.py
@@ -32,6 +32,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "numpy": ("https://numpy.org/doc/stable/", None),
     "sympy": ("https://docs.sympy.org/latest/", None),
+    "numba": ("https://numba.readthedocs.io/en/stable/", None),
 }
 
 templates_path = ["_templates"]
